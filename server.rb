1fdeadf52e06e0e8a1a74d44b9133d23a1052d48
Karl Eklund <localpart@gmail.com>
2015-03-27 20:11:25 +0100

    Refactor

--- server.rb
+++ server.rb
@@ -2,28 +2,51 @@
 
 require "socket"
  
-HEADER = "HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\n"
-port = ENV["PORT"] or raise "Must set PORT environment variable"
-server = TCPServer.new port
+class WebServer
+  HEADER = "HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\n"
 
-loop do
-  begin
-    Thread.start(server.accept) do |socket|
-      request = []
-      while line = socket.gets
-        break if line =~ /^\s*$/
-        request << line.chomp
+  def initialize(port, &handler)
+    @port = port
+    @handler = handler
+  end
+
+  def start
+    server = TCPServer.new port
+
+    loop do
+      Thread.start(server.accept) do |socket|
+        begin
+          request = receive_request!(socket)
+          socket.print HEADER
+          handler.call(socket, request)
+          socket.close
+        rescue Errno::EPIPE
+          retry
+        end
       end
-
-      socket.print HEADER
-
-      socket.puts "Hello world! #{Time.now}"
-      socket.puts
-      socket.puts request
-
-      socket.close
     end
-  rescue Errno::EPIPE
-    retry
+  end
+
+  private
+
+  attr_reader :port, :handler
+
+  def receive_request!(socket)
+    request = []
+    while line = socket.gets
+      break if line =~ /^\s*$/
+      request << line.chomp
+    end
+    request
   end
 end
+
+port = ENV["PORT"] or raise "Must set PORT environment variable"
+
+web_server = WebServer.new(port) do |socket, request|
+  socket.puts "Hello world! #{Time.now}"
+  socket.puts
+  socket.puts request
+end
+
+web_server.start
