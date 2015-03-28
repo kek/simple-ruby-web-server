a7cc8d4d276af352964d5dc7a19791da4d0512bd
Karl Eklund <localpart@gmail.com>
2015-03-28 19:52:39 +0100

    Revert "Refactor"
    This reverts commit 1fdeadf52e06e0e8a1a74d44b9133d23a1052d48.

--- server.rb
+++ server.rb
@@ -2,51 +2,28 @@
 
 require "socket"
  
-class WebServer
-  HEADER = "HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\n"
-
-  def initialize(port, &handler)
-    @port = port
-    @handler = handler
-  end
-
-  def start
-    server = TCPServer.new port
-
-    loop do
-      begin
-        Thread.start(server.accept) do |socket|
-          request = receive_request!(socket)
-          socket.print HEADER
-          handler.call(socket, request)
-          socket.close
-        end
-      rescue Errno::EPIPE, IOError
-        retry
-      end
-    end
-  end
-
-  private
-
-  attr_reader :port, :handler
-
-  def receive_request!(socket)
-    request = []
-    while line = socket.gets
-      break if line =~ /^\s*$/
-      request << line.chomp
-    end
-    request
-  end
-end
-
+HEADER = "HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\n"
 port = ENV["PORT"] or raise "Must set PORT environment variable"
+server = TCPServer.new port
 
-web_server = WebServer.new(port) do |socket, request|
-  socket.puts "Hello world! #{Time.now}"
-  socket.puts
-  socket.puts request
+loop do
+  begin
+    Thread.start(server.accept) do |socket|
+      request = []
+      while line = socket.gets
+        break if line =~ /^\s*$/
+        request << line.chomp
+      end
+
+      socket.print HEADER
+
+      socket.puts "Hello world! #{Time.now}"
+      socket.puts
+      socket.puts request
+
+      socket.close
+    end
+  rescue Errno::EPIPE, IOError
+    retry
+  end
 end
-
-web_server.start
